34605e50c123c585d5756056fa9da1183ee63588
Neum <1975584042@qq.com>
2020-10-25 22:56:22 +0800

    init commit

---
+++ Assets/Script/Controller/MoveController.cs
@@ -0,0 +1,19 @@
+﻿// 移动控制者
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : ControllerBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
